f592c6b10f82d81eefbe38ca91843a09b9aa685c
sukhpal0525 <sukhpal.shergill1103@gmail.com>
2024-09-25 01:22:06 +0100

    Setup App component

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,22 +1,23 @@
 import React, { useState } from 'react';
+import Header from './components/Header';
+import TodoForm from './components/TodoForm';
+import TodoList from './components/TodoList';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+
+  // Function to add a new task
+  const addTask = (newTask) => {
+    setTasks([...tasks, { taskName: newTask, completed: false }]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mt-5">
+      <Header />
+      <TodoForm addTask={addTask} />
+      <TodoList tasks={tasks} />
     </div>
   );
 }
